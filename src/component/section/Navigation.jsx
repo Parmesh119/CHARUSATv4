fd9fa1fc114a52b7d7cc79fa045cce7a18129a63
Ravi Patel <ravifromchanga@gmail.com>
2024-01-23 13:55:48 +0530

    new update 1

--- src/component/section/Navigation.jsx
+++ src/component/section/Navigation.jsx
@@ -106,10 +106,7 @@ const Header = () => {
                     Research
                   </a>
                 </li>
-                <li
-                  className="link"
-                  
-                >
+                <li className="link">
                   <a
                     id="IQAC-link"
                     target="_blank"
@@ -118,16 +115,13 @@ const Header = () => {
                     IQAC
                   </a>
                 </li>
-                <li
-                  className="link"
-                  
-                >
+                <li className="link">
                   <a
                     id="IQAC-link"
                     target="_blank"
                     href="https://charusat.online/"
                   >
-                  Online Programmes
+                    Online Programmes
                   </a>
                 </li>
                 <li
@@ -219,11 +213,7 @@ const Header = () => {
                   BCA
                 </Link>
                 <Link to="./course-single#2402" className="menu-link">
-<<<<<<< HEAD
                   B.Sc.(IT)
-=======
-                  B.Sc(IT)
->>>>>>> main
                 </Link>
                 <Link to="./course-single#3401" className="menu-link">
                   B.Pharm (Bachelor of Pharmacy)
@@ -232,27 +222,18 @@ const Header = () => {
                   BBA
                 </Link>
                 <Link to="./course-single#5401" className="menu-link">
-<<<<<<< HEAD
                   B.Sc.(Biology)
-=======
-                  B.Sc(Biology)
->>>>>>> main
                 </Link>
                 <Link to="./course-single#6401" className="menu-link">
                   BPT(Bachelor of Physiotherapy)
                 </Link>
                 <Link to="./course-single#6402" className="menu-link">
-<<<<<<< HEAD
                   B.Sc.(Nursing)
-=======
-                  B.Sc(Nursing)
->>>>>>> main
                 </Link>
                 <Link to="./course-single#6405" className="menu-link">
                   Bachelor of Optometry (B.Optom.)
                 </Link>
                 <Link to="./course-single#6403" className="menu-link">
-<<<<<<< HEAD
                   B.Sc. (Imaging Technology)
                 </Link>
                 <Link to="./course-single#6406" className="menu-link">
@@ -264,19 +245,6 @@ const Header = () => {
 
                 <Link to="./course-single#5401" className="menu-link">
                   B.Sc. (Microbiology, Biotechnology, Biochemistry)
-=======
-                  B.Sc (Imaging Technology)
-                </Link>
-                <Link to="./course-single#6406" className="menu-link">
-                  B.Sc (Medical Technnology)
-                </Link>
-                <Link to="./course-single#6404" className="menu-link">
-                  B.Sc (Operation Theatre & Anesthesia Technology)
-                </Link>
-
-                <Link to="./course-single#5401" className="menu-link">
-                  B.Sc (Microbiology, Biotechnology, Biochemistry)
->>>>>>> main
                 </Link>
               </div>
             </div>
@@ -290,11 +258,7 @@ const Header = () => {
                   MCA
                 </Link>
                 <Link to="./course-single#2502" className="menu-link">
-<<<<<<< HEAD
                   M.Sc.(IT)
-=======
-                  M.Sc(IT)
->>>>>>> main
                 </Link>
                 <Link to="./course-single#3501" className="menu-link">
                   M.Pharm
@@ -303,7 +267,6 @@ const Header = () => {
                   MBA
                 </Link>
                 <Link to="./course-single#5505" className="menu-link">
-<<<<<<< HEAD
                   M.Sc. (Advanced Organic Chemistry)
                 </Link>
                 <Link to="./course-single#5502" className="menu-link">
@@ -317,27 +280,11 @@ const Header = () => {
                 </Link>
                 <Link to="./course-single#5506" className="menu-link">
                   M.Sc. (Physics)
-=======
-                  M.Sc (Advanced Organic Chemistry)
-                </Link>
-                <Link to="./course-single#5502" className="menu-link">
-                  M.Sc (Biochemistry)
-                </Link>
-                <Link to="./course-single#5501" className="menu-link">
-                  M.Sc (Biotechnology)
-                </Link>
-                <Link to="./course-single#5503" className="menu-link">
-                  M.Sc (Microbiology)
-                </Link>
-                <Link to="./course-single#5506" className="menu-link">
-                  M.Sc (Physics)
->>>>>>> main
                 </Link>
                 <Link to="./course-single#6501" className="menu-link">
                   MPT (Master of Physiotherapy)
                 </Link>
                 <Link to="./course-single#6510" className="menu-link">
-<<<<<<< HEAD
                   M.Sc. (Nursing)
                 </Link>
                 <Link to="./course-single#6514" className="menu-link">
@@ -348,18 +295,6 @@ const Header = () => {
                 </Link>
                 <Link to="./course-single#6513" className="menu-link">
                   M.Sc. (Maths)
-=======
-                  M.Sc (Nursing)
-                </Link>
-                <Link to="./course-single#6514" className="menu-link">
-                  M.Sc (Medical Imaging Technology)
-                </Link>
-                <Link to="./course-single#6513" className="menu-link">
-                  M.Sc (Medical Laboratory Technology)
-                </Link>
-                <Link to="./course-single#6513" className="menu-link">
-                  M.Sc (Maths)
->>>>>>> main
                 </Link>
               </div>
             </div>
@@ -887,11 +822,7 @@ const Header = () => {
                     University Officers
                   </Link>
                   <Link to="./journey" className="menu-link">
-<<<<<<< HEAD
-                   Journey of Charusat
-=======
                     Journey of Charusat
->>>>>>> main
                   </Link>
                 </div>
               </div>
